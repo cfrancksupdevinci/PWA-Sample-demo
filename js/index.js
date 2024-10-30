31c8f0ca090e9ca871181e70e34571fccdfd5094
cfrancksupdevinci <cyrille.franck@supdevinci-edu.fr>
2024-10-30 11:58:50 +0100

    display notif

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
+    if (!("Notification" in window)) {
+      console.log("This browser does not support notifications.");
+      return;
+    }
+    Notification.requestPermission().then((permission) => {
+      // On affiche ou non le bouton en fonction de la réponse
+      notificationBtn.style.display =
+        permission === "granted" ? "none" : "block";
+    });
     navigator.serviceWorker.register("sw.js").then((swRegistered) => {
       console.log("[ServiceWorker**] - Registered");
     });
